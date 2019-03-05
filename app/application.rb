a149034b118264337d72f411ec06af79b37f4b26
Jared Harbison <jared.harbison@gmail.com>
2019-03-05 17:41:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items").last
-      if item = @@items.find{|i| i.name == item_name}
-        resp.write item.price
+      if item = @@items.find {|i| i.name == item_name}
+        resp.write "#{item.price}
       else
         resp.write "Item not found"
         resp.status = 400
@@ -19,4 +19,4 @@ class Application
     end
     resp.finish
   end
-end 
+end
